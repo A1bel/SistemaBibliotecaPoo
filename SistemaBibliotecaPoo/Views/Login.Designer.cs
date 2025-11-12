5348cd4f68e63f2cdae5f8e4083072b02160ccdc
lilian borsoi <lilianborsoi@icloud.com>
2025-11-12 20:15:43 -0300

    feat: implementar tela de login

---
+++ SistemaBibliotecaPoo/Views/Login.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SistemaBibliotecaPoo
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailTxt = new System.Windows.Forms.TextBox();
+            this.senhaTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.entrarBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // emailTxt
+            // 
+            this.emailTxt.Location = new System.Drawing.Point(181, 108);
+            this.emailTxt.Name = "emailTxt";
+            this.emailTxt.Size = new System.Drawing.Size(100, 22);
+            this.emailTxt.TabIndex = 1;
+            // 
+            // senhaTxt
+            // 
+            this.senhaTxt.Location = new System.Drawing.Point(181, 176);
+            this.senhaTxt.Name = "senhaTxt";
+            this.senhaTxt.Size = new System.Drawing.Size(100, 22);
+            this.senhaTxt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Senha";
+            // 
+            // entrarBtn
+            // 
+            this.entrarBtn.Location = new System.Drawing.Point(181, 246);
+            this.entrarBtn.Name = "entrarBtn";
+            this.entrarBtn.Size = new System.Drawing.Size(75, 23);
+            this.entrarBtn.TabIndex = 4;
+            this.entrarBtn.Text = "Entrar";
+            this.entrarBtn.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.entrarBtn);
+            this.Controls.Add(this.senhaTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.emailTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox emailTxt;
+        private System.Windows.Forms.TextBox senhaTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button entrarBtn;
+    }
+}
+
