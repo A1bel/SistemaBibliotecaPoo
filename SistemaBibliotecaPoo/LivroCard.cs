ee5573366486ffa7856e3ecb7314149226b179e5
A1bel <arthur.aibel1@gmail.com>
2025-11-19 21:18:22 -0300

    feat: Terminar implementação da funcionalidade da Home e da LivroCard, e adicionar campo preço no Livro

--- SistemaBibliotecaPoo/LivroCard.cs
+++ SistemaBibliotecaPoo/LivroCard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaBibliotecaPoo.Models.Usuario;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,21 @@ namespace SistemaBibliotecaPoo
         public LivroCard()
         {
             InitializeComponent();
+        }
+
+        public void SetData(Livro livro)
+        {
+            tituloLbl.Text = livro.Titulo;
+            precoLbl.Text = livro.Preco.ToString();
+            autorLbl.Text = livro.Autor;
+            categoriaLbl.Text = livro.Categoria;
+
+            this.Tag = livro.Id;
+        }
+
+        private void detalhesBtn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
