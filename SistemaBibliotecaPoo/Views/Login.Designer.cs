8c15d149cbf0cd81e5fc281917be7549cebb9c48
lilian borsoi <lilianborsoi@icloud.com>
2025-11-21 15:37:27 -0300

    feat: tela login implementada

--- SistemaBibliotecaPoo/Views/Login.Designer.cs
+++ SistemaBibliotecaPoo/Views/Login.Designer.cs
@@ -34,9 +34,11 @@
             this.emailTxt = new System.Windows.Forms.TextBox();
             this.senhaTxt = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.contaLbl = new System.Windows.Forms.LinkLabel();
+            this.titulo = new System.Windows.Forms.Label();
             this.entrarBtn = new System.Windows.Forms.Button();
             this.erroLbl = new System.Windows.Forms.Label();
-            this.contaLbl = new System.Windows.Forms.LinkLabel();
+            this.erroSenhaLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // imageList1
@@ -48,86 +50,111 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(150, 72);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(230, 136);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(60, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "Email";
             // 
             // emailTxt
             // 
-            this.emailTxt.Location = new System.Drawing.Point(136, 88);
-            this.emailTxt.Margin = new System.Windows.Forms.Padding(2);
+            this.emailTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailTxt.Location = new System.Drawing.Point(235, 172);
+            this.emailTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.emailTxt.Name = "emailTxt";
-            this.emailTxt.Size = new System.Drawing.Size(76, 20);
+            this.emailTxt.Size = new System.Drawing.Size(330, 30);
             this.emailTxt.TabIndex = 1;
             // 
             // senhaTxt
             // 
-            this.senhaTxt.Location = new System.Drawing.Point(136, 143);
-            this.senhaTxt.Margin = new System.Windows.Forms.Padding(2);
+            this.senhaTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.senhaTxt.Location = new System.Drawing.Point(235, 267);
+            this.senhaTxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.senhaTxt.Name = "senhaTxt";
-            this.senhaTxt.Size = new System.Drawing.Size(76, 20);
+            this.senhaTxt.Size = new System.Drawing.Size(330, 30);
             this.senhaTxt.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(150, 128);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(230, 229);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(70, 25);
             this.label2.TabIndex = 2;
             this.label2.Text = "Senha";
-            // 
-            // entrarBtn
-            // 
-            this.entrarBtn.Location = new System.Drawing.Point(136, 200);
-            this.entrarBtn.Margin = new System.Windows.Forms.Padding(2);
-            this.entrarBtn.Name = "entrarBtn";
-            this.entrarBtn.Size = new System.Drawing.Size(56, 19);
-            this.entrarBtn.TabIndex = 4;
-            this.entrarBtn.Text = "Entrar";
-            this.entrarBtn.UseVisualStyleBackColor = true;
-            this.entrarBtn.Click += new System.EventHandler(this.entrarBtn_Click);
-            // 
-            // erroLbl
-            // 
-            this.erroLbl.AutoSize = true;
-            this.erroLbl.ForeColor = System.Drawing.Color.Red;
-            this.erroLbl.Location = new System.Drawing.Point(133, 22);
-            this.erroLbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.erroLbl.Name = "erroLbl";
-            this.erroLbl.Size = new System.Drawing.Size(0, 13);
-            this.erroLbl.TabIndex = 5;
             // 
             // contaLbl
             // 
             this.contaLbl.AutoSize = true;
-            this.contaLbl.Location = new System.Drawing.Point(133, 237);
+            this.contaLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contaLbl.Location = new System.Drawing.Point(231, 326);
+            this.contaLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.contaLbl.Name = "contaLbl";
-            this.contaLbl.Size = new System.Drawing.Size(110, 13);
+            this.contaLbl.Size = new System.Drawing.Size(168, 20);
             this.contaLbl.TabIndex = 7;
             this.contaLbl.TabStop = true;
             this.contaLbl.Text = "Não tenho uma conta";
             this.contaLbl.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.contaLbl_LinkClicked);
             // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(285, 38);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(253, 39);
+            this.titulo.TabIndex = 19;
+            this.titulo.Text = "Acessar Conta";
+            // 
+            // entrarBtn
+            // 
+            this.entrarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entrarBtn.ForeColor = System.Drawing.Color.Black;
+            this.entrarBtn.Location = new System.Drawing.Point(552, 350);
+            this.entrarBtn.Name = "entrarBtn";
+            this.entrarBtn.Size = new System.Drawing.Size(134, 38);
+            this.entrarBtn.TabIndex = 20;
+            this.entrarBtn.Text = "Entrar";
+            this.entrarBtn.UseVisualStyleBackColor = true;
+            // 
+            // erroLbl
+            // 
+            this.erroLbl.AutoSize = true;
+            this.erroLbl.ForeColor = System.Drawing.Color.Red;
+            this.erroLbl.Location = new System.Drawing.Point(232, 204);
+            this.erroLbl.Name = "erroLbl";
+            this.erroLbl.Size = new System.Drawing.Size(0, 16);
+            this.erroLbl.TabIndex = 21;
+            // 
+            // erroSenhaLbl
+            // 
+            this.erroSenhaLbl.AutoSize = true;
+            this.erroSenhaLbl.ForeColor = System.Drawing.Color.Red;
+            this.erroSenhaLbl.Location = new System.Drawing.Point(232, 299);
+            this.erroSenhaLbl.Name = "erroSenhaLbl";
+            this.erroSenhaLbl.Size = new System.Drawing.Size(0, 16);
+            this.erroSenhaLbl.TabIndex = 22;
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.contaLbl);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.erroSenhaLbl);
             this.Controls.Add(this.erroLbl);
             this.Controls.Add(this.entrarBtn);
+            this.Controls.Add(this.titulo);
+            this.Controls.Add(this.contaLbl);
             this.Controls.Add(this.senhaTxt);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.emailTxt);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,9 +167,11 @@
         private System.Windows.Forms.TextBox emailTxt;
         private System.Windows.Forms.TextBox senhaTxt;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel contaLbl;
+        private System.Windows.Forms.Label titulo;
         private System.Windows.Forms.Button entrarBtn;
         private System.Windows.Forms.Label erroLbl;
-        private System.Windows.Forms.LinkLabel contaLbl;
+        private System.Windows.Forms.Label erroSenhaLbl;
     }
 }
 
